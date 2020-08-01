f0c8cdc20c42f451a8e9d06e175db898bcc61bef
Nesibe64 <sabancinesibe@gmail.com>
2020-08-01 11:56:49 -0400

    update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -1,6 +1,11 @@
 public class HappyB20 {
     public static void main(String[] args) {
+
         System.out.println("I love Hakki, he bought me MAcBOOK");
         System.out.println("Hakki");
+        System.out.println("Nesibe sABANCI");
+
+
+
     }
 }
