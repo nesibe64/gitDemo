3ff1ee3c89adb60450e03e20019a17d40d20be21
Nesibe64 <sabancinesibe@gmail.com>
2020-08-01 11:39:06 -0400

    add HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -1,5 +1,6 @@
 public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("I love Hakki, he bought me MAcBOOK");
+        System.out.println("Hakki");
     }
 }
